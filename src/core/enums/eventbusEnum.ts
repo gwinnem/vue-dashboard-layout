b23668ffac735ab6524f4ab3d983b3970654e8f9
Geirr Winnem <g.winnem@ringana.com>
2022-04-30 19:18:15 +0200

    Enums for magic strings

---
+++ src/core/enums/eventbusEnum.ts
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+// eslint-disable-next-line no-shadow
+enum EEventbusEnums {
+    RESIZE_EVENT = "resizeEvent",
+    DRAG_EVENT = "dragEvent",
+    UPDATE_WIDTH="updateWidth",
+    SET_COL_NUM="setColNum",
+    SET_ROW_HEIGHT="setRowHeight",
+    SET_DRAGGABLE="setDraggable",
+    SET_RESIZABLE="setResizable",
+    SET_MAX_ROWS="setMaxRows",
+    COMPACT="compact",
+    DIRECTION_CHANGE="directionchange",
+}
